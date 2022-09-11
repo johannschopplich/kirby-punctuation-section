9de12ac4ba9a59b721a7b0cc40ece35445096f74
Johann Schopplich <mail@johannschopplich.com>
2022-09-11 17:41:30 +0200

    refactor: simplify index script

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
 <?php
 
-$base = \Kirby\Cms\Section::$types['info'];
-
-if (is_string($base)) {
-    $base = include $base;
-}
-
 \Kirby\Cms\App::plugin('johannschopplich/punctuation-section', [
     'sections' => [
-        'punctuation' => array_replace_recursive($base, [
-            'props' => [
-                'fieldsets' => fn (array $value = []) => $value
+        'punctuation' => array_replace_recursive(
+            include \Kirby\Cms\Section::$types['info'],
+            [
+                'props' => [
+                    'fieldsets' => fn (array $value = []) => $value
+                ]
             ]
-        ])
+        )
     ]
 ]);
