a9591d87fc29e35fd02be05baab597491a2e9335
Johann Schopplich <mail@johannschopplich.com>
2022-12-18 13:22:23 +0100

    fix: support section `label` blueprint for Kirby 3.7+

--- src/components/PunctuationSection.vue
+++ src/components/PunctuationSection.vue
@@ -54,7 +54,7 @@ export default {
 
   data() {
     return {
-      headline: null,
+      label: null,
       text: [],
       theme: null,
       activeChar: null,
@@ -64,7 +64,7 @@ export default {
 
   async created() {
     const response = await this.load();
-    this.headline = response.headline;
+    this.label = response.label || response.headline;
     this.theme = response.theme || "none";
     this.text = this.fieldsets.map((i) => ({
       ...i,
